af94ad9ed8cd9de4a34967844223c7f6aa657200
deepKnight <969309843@qq.com>
2016-12-23 23:15:30 +0800

    在HTML中使用循环

--- views.py
+++ views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "index.html")
+    block_infos = [{"name": "日常专区", "desc": "一个栗子", "manager": "admin"},
+                   {"name": "战术交流", "desc": "一个栗子", "manager": "admin"},
+                   {"name": "论坛建设", "desc": "一个栗子", "manager": "admin"},]
+    return render(request, "index.html", {"blocks":block_infos})
