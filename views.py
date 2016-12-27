7498372d6c75c8168d0aa7a21327bc88f734ebdf
deepKnight <969309843@qq.com>
2016-12-27 13:47:13 +0800

    创建admin和数据表
    application 数据表 admin

--- views.py
+++ views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from block.models import Block
 
 def index(request):
-    block_infos = [{"name": "日常专区", "desc": "一个栗子", "manager": "admin"},
-                   {"name": "战术交流", "desc": "一个栗子", "manager": "admin"},
-                   {"name": "论坛建设", "desc": "一个栗子", "manager": "admin"},]
+    # block_infos = [{"name": "日常专区", "desc": "一个栗子", "manager": "admin"},
+    #                {"name": "战术交流", "desc": "一个栗子", "manager": "admin"},
+    #                {"name": "论坛建设", "desc": "一个栗子", "manager": "admin"},]
+    block_infos = Block.objects.all().order_by("id")
     return render(request, "index.html", {"blocks":block_infos})
