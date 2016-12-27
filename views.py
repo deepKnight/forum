a1234a9c325f88ce3927e2b95df96eeaef381971
deepKnight <969309843@qq.com>
2016-12-27 15:21:50 +0800

    添加一列
    filter()用来筛选
    eg, status__lt=0; status__gte=0;

--- views.py
+++ views.py
@@ -6,5 +6,5 @@ def index(request):
     # block_infos = [{"name": "日常专区", "desc": "一个栗子", "manager": "admin"},
     #                {"name": "战术交流", "desc": "一个栗子", "manager": "admin"},
     #                {"name": "论坛建设", "desc": "一个栗子", "manager": "admin"},]
-    block_infos = Block.objects.all().order_by("id")
+    block_infos = Block.objects.filter(status=0).order_by("id")
     return render(request, "index.html", {"blocks":block_infos})
