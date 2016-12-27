f6ec988a252cbffcc933cc3a4348d274fa69371b
deepKnight <969309843@qq.com>
2016-12-27 14:07:07 +0800

    美化admin
    中文表现admin，丰富admin

--- block/models.py
+++ block/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 class Block(models.Model):
-    name = models.CharField(u"版块名称", max_length=100)
-    desc = models.CharField(u"版块描述", max_length=100)
-    manager_name = models.CharField(u"管理员名称", max_length=100)
+    name = models.CharField("版块名称", max_length=100)
+    desc = models.CharField("版块描述", max_length=100)
+    manager_name = models.CharField("管理员名称", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "版块"
+        verbose_name_plural = "这些版块"
